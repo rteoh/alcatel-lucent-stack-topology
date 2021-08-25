1274145e464a4009fa6011d056c0d6c6029babc2
rteoh <43252547+rteoh@users.noreply.github.com>
2021-08-24 18:27:08 -0700

    Update stack-topology.py

--- stack-topology.py
+++ stack-topology.py
@@ -294,7 +294,7 @@ except ValueError:
 
 	print('\n')
 	print('Error: Invalid IP Address')
-	print('Usage: python stack-topology.py <ip or .txt file> -output [Output file]')
+	print('Usage: python stack-topology.py <ip or .txt file> -output')
 	exit()
 
 except SystemExit:
@@ -304,5 +304,5 @@ except SystemExit:
 except:
 
 	print('\n')
-	print('Usage: python stack-topology.py <ip or .txt file> -output [Output file]')
+	print('Usage: python stack-topology.py <ip or .txt file> -output')
 	exit()
